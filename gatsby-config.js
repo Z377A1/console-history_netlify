47e7d83bd7c1cf42f0cd19d75835421f525681eb
Z377A1 <70942307+Z377A1@users.noreply.github.com>
2023-11-02 04:16:53 +0200

    added blog posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,11 +6,19 @@ module.exports = {
     title: "History of Consoles",
     description: `Example project for the Gatsby Head API`,
     twitterUsername: `@ZettaiDao`,
-    image: `./static/favicon.png`,
+    image: `./static/favicon.png`, // does not show
     siteUrl: `https://www.historyofconsoles.com`,
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+    "gatsby-plugin-mdx",
   ],
 }
